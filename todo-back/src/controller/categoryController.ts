71cad4e75e1d0515e2018fcd8213bf7cbff04fd5
mohammad amin jafari <m.a.jafari002@gmail.com>
2024-06-12 17:06:38 +0330

    nothing

--- todo-back/src/controller/categoryController.ts
+++
@@ -1,4 +0,0 @@
-import { categoryModel } from "../Model/categoryModel";
-import { getAll } from "./factoryFn";
-
-export const getCategories = getAll(categoryModel);
--- todo-back/src/lib/bcrypt.ts
+++
@@ -1,6 +0,0 @@
-import { hash, compare } from "bcryptjs";
-export const hashPassword = async (password: string) =>
-  await hash(password, 12);
-
-export const compareHashed = async (password: string, hashed: string) =>
-  await compare(password, hashed);
